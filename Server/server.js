24b29fadbe79afb411471758e5d0d946d13dfad0
Bhuwan001 <sutharbhuwan2@gmail.com>
2026-05-10 17:57:29 +0530

    10-may

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,7 @@
 // main server file
 
+// MONGO_URL = "mongodb://localhost:27017/WorkoutApp"
+
 // setting up the server
 const express = require("express");
 const app = express();
@@ -9,7 +11,6 @@ require("dotenv").config();
 const connectDB = require("./config/db")
 
 connectDB();
-
 
 app.get("/setupcheck", function(req, res){
 
