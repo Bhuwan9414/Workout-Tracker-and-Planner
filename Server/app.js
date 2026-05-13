be58739f3114c059b73ccd0819727e0686574679
System Administrator <administrator@f4-desktop-57.terragrit.io>
2026-05-13 18:18:51 +0530

    latest commit

--- Server/app.js
+++ Server/app.js
@@ -1,11 +1,17 @@
+// importing the EXPRESS packages (bringing express lib here)
 const express = require("express");
 
+// importing the auth Routes
 const authRoutes = require("./routes/authRoutes");
 
+//  creating the express app after this we can use all express methods
 const app = express();
 
+// parses the incoming json in req.body to js object
+// app.use is used to run middlewares between request and response
 app.use(express.json());
 
+//  using tthe auth route
 app.use("/api/auth", authRoutes);
 
 module.exports = app;
--- Server/server.js
+++ Server/server.js
@@ -1,19 +1,18 @@
 // main server file
 
-// MONGO_URL = "mongodb://localhost:27017/WorkoutApp"
-
-// setting up the server
-// const express = require("express");
-
+//  reads the variables declared in the .env files and loads the value in process.env
 require("dotenv").config();
 
+// importing the express app from app.js file
 const app = require("./app")
 
-
+// imoirting the connectDB function from db.js
 const connectDB = require("./config/db")
 
+// calling the connectDB for connecting to database
 connectDB();
 
+// exposing the app on localhost:3000
 app.listen(process.env.PORT, function(){
     console.log("server is listening on port  " + process.env.PORT);
 })
