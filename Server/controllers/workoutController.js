8bce5eb9a1e8f35b52ae6c9d8b9e96fb8a5cddd7
Bhuwan001 <sutharbhuwan2@gmail.com>
2026-07-06 09:20:10 +0530

    new commit

--- Server/controllers/workoutController.js
+++ Server/controllers/workoutController.js
@@ -1,7 +1,30 @@
-const startWorkoutService = require("../services/workoutService");
+const {startWorkoutService} = require("../services/workoutService");
 
 const startWorkoutController = async function(req, res){
 
-    
+    try {
 
+        const routineId = req.body.routineId;
+        const userId =  req.user.id
+
+        const workout = await startWorkoutService.startWorkoutService(routineId, userId)
+
+        res.status(201).json({
+            message : "workout initialised successfully",
+            workout
+        })
+
+    }
+     catch (error) {
+
+        // if any error occurs then return error message
+        res.status(400).json({
+            message: error.message
+        })
+    }
+
+}
+
+module.exports = {
+    startWorkoutController
 }
---
+++ Server/routes/workoutRoutes.js
@@ -0,0 +1,9 @@
+const startWorkoutController = require("../controllers/workoutController");
+
+const authMiddleware = require("../middlewares/authMiddleware")
+
+const express = require("express");
+
+const router = express.Router();
+
+router.post("/startWorkout",)
