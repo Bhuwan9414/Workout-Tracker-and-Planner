7e8370cb30dac780e2d0fe9ee66f96ea84d9436a
Bhuwan Suthar <sutharbhuwan2@gmail.com>
2026-07-31 17:46:13 +0530

    update: 2026-07-31 17:46

--- frontend/src/Routes/appRoutes.jsx
+++ frontend/src/Routes/appRoutes.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Login from "../pages/Login";
-import Register from "../pages/Register";
-import Dashboard from "../pages/Dashboard";
+import Login from "../pages/login";
+import Register from "../pages/register";
+import Dashboard from "../pages/dashboard";
 
 const AppRoutes = () => {
   return (
