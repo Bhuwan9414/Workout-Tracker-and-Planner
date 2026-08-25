d15b7075d2dba0da7d14edb87478ee4b3d75cfdf
Bhuwan001 <sutharbhuwan2@gmail.com>
2026-08-25 22:42:24 +0530

    update: 2026-08-25 22:42

--- Server/config/db.js
+++ Server/config/db.js
@@ -2,10 +2,18 @@ const mongoose = require("mongoose");
 
 // connect to database
 
+        // console.log(process.env.MONGO_URL);
+
+        const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
+
 async function connectDB(){
 
     try {
         await mongoose.connect(process.env.MONGO_URL)
+        // console.log(process.env.MONGO_URL);
+        
         console.log("database conection successfull");
         
     }
